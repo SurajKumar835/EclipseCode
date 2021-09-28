cc6b43c5beeed5218181af9bf899db2157fdc980
Suraj <surajkumar.kumar835@gmail.com>
2021-09-28 12:43:34 +0530

    Commit_1

---
+++ src/pkg_1/School.java
@@ -0,0 +1,12 @@
+package pkg_1;
+
+public class School {
+		String name="K V Khagaria";
+		int rollno=23;
+		String classNo="10Th";
+		
+		public void displayDetails()
+		{
+			System.out.println( "Name is "+ this.name + "Roll No is "+ this.rollno+"Class Studying in "+this.classNo);
+		}
+}
---
+++ src/pkg_1/SchoolMain.java
@@ -0,0 +1,11 @@
+package pkg_1;
+
+public class SchoolMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			School s=new School();
+			s.displayDetails();
+	}
+
+}
