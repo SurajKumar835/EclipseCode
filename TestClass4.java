05d60721a6c7a5c178f7320fed429c954b298e75
Surajkumar <surajkumar.kumar835@gmail.com>
2021-09-30 10:50:46 +0530

    Commit_7

---
+++ TestClass4.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class TestClass4 {
+    public static void main(String args[]) throws Exception{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T=Integer.parseInt(br.readLine());
+        for(int i=0;i<T;i++)
+        {
+            String str=br.readLine();
+            String t="";
+            for(int k=0;k<str.length();k++)
+            {
+                t=str.charAt(k)+t;
+            }
+            //add both original and reverse strings
+            int a='a';
+            char[] oA=str.toCharArray();
+            char[] cA=t.toCharArray();
+            int[] cAi=new int[str.length()];
+            for(int j=0;j<cA.length;j++)
+            {
+                cAi[j]=cA[j]+oA[j]-a+1;
+                if(cAi[j]>122)
+                {
+                    cAi[j]=a+cAi[j]-123;
+                }
+                System.out.print((char)cAi[j]);
+            }
+            System.out.print("\n");
+        }
+    }
+}
