8742eaa920c795c42436f8721da0a5b87603dee7
Suraj <surajkumar.kumar835@gmail.com>
2021-09-30 11:18:40 +0530

    Commit_9

--- src/pkg_1/Reverse.java
+++ src/pkg_1/Reverse.java
@@ -11,7 +11,7 @@ public class Reverse {
         int N=Integer.parseInt(br.readLine());
         String s=br.readLine();
         String t=br.readLine();
-        if(s.length()==t.length())
+        if(N==t.length())
         {
             char s1[]=s.toCharArray();
             char t1[]=t.toCharArray();
