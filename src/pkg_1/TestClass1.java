b4d7bab4b4d13070d0ca165a480b794383b6d6c4
Suraj <surajkumar.kumar835@gmail.com>
2021-09-30 11:27:18 +0530

    Shifted under pkg_1

--- src/pkg_1/TestClass1.java
+++ src/pkg_1/TestClass1.java
@@ -1,32 +1,27 @@
+package pkg_1;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-public class TestClass2 {
+public class TestClass1 {
     public static void main(String args[] ) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T=Integer.parseInt(br.readLine());
         for(int i=0;i<T;i++)
         {
             String s= br.readLine(); 
-            int[] arr=new int[s.length()];String t="";int c=0;
+            // Write your code here
+            String t="";
             for(int l=0;l<s.length();l++)
             {
                if(s.charAt(l)!=' ')
                {
                    t=s.charAt(l)+t;
                }
-               else{
-                   arr[c]=l;c++;
-               }
             }
-            StringBuilder sb=new StringBuilder(t);
-            for(int j=0;j<t.length();j++)
-            {
-                if(arr[j]!=0)
-                {
-                    sb.insert(arr[j]," ");
-                }
-            }
-            System.out.println(sb.toString());
+            int k=s.indexOf(' ');
+            String s1=t.substring(0,k);
+            String s2=t.substring(k,t.length());
+            System.out.println(s1+" "+s2);
             
         }
     }
--- src/pkg_1/TestClass5.java
+++ src/pkg_1/TestClass5.java
@@ -1,3 +1,5 @@
+package pkg_1;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 public class TestClass5 {
